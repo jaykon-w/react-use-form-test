1dd5845ee9aaabddeb9fd18837f622096beccba3
jaykon-w <jaykon.w@gmail.com>
2019-12-12 11:36:07 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import Hello from "./Hello";
 import "./style.css";
 import { useForm, useField } from "./formHooks";
 import { stringMask } from "stringmask-js";
